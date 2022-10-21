d2e95c69343e1e324c9987868a7442dd191668b1
Subaraki <robbeenaxel@hotmail.com>
2022-10-21 11:17:42 +0200

    add carcass world gen feature for fabric

--- Fabric/src/main/java/net/skeletoncrew/bonezone/BoneZoneFabric.java
+++ Fabric/src/main/java/net/skeletoncrew/bonezone/BoneZoneFabric.java
@@ -1,12 +1,13 @@
 package net.skeletoncrew.bonezone;
 
 import net.fabricmc.api.ModInitializer;
+import net.skeletoncrew.bonezone.feature.CarcassFeatureRegistry;
 
 public class BoneZoneFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
         new BoneZoneCommon();
+        CarcassFeatureRegistry.INSTANCE.register();
     }
 }
