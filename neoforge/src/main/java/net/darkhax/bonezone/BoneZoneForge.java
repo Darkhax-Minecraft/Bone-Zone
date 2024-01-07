386c9763d55e98bf41eb7b2c2fe3ee50314d1113
Tyler Hancock <darklime@live.ca>
2024-01-07 07:57:28 -0700

    Update registry code.

--- neoforge/src/main/java/net/darkhax/bonezone/BoneZoneForge.java
+++ neoforge/src/main/java/net/darkhax/bonezone/BoneZoneForge.java
@@ -1,11 +1,11 @@
 package net.darkhax.bonezone;
 
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.ForgeRegistries;
-import net.neoforged.neoforge.registries.RegistryObject;
 import net.skeletoncrew.bonezone.BoneZoneCommon;
 import net.skeletoncrew.bonezone.Constants;
 import net.skeletoncrew.bonezone.feature.CarcassFeature;
@@ -13,10 +13,10 @@ import net.skeletoncrew.bonezone.feature.CarcassFeature;
 @Mod(Constants.MOD_ID)
 public class BoneZoneForge {
 
-    public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, Constants.MOD_ID);
-    public static final RegistryObject<Feature<?>> BIRD = FEATURES.register("carcass_bird", CarcassFeature::new);
-    public static final RegistryObject<Feature<?>> DEER = FEATURES.register("carcass_deer", CarcassFeature::new);
-    public static final RegistryObject<Feature<?>> GOAT = FEATURES.register("carcass_goat", CarcassFeature::new);
+    public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(Registries.FEATURE, Constants.MOD_ID);
+    public static final DeferredHolder<Feature<?>, Feature<?>> BIRD = FEATURES.register("carcass_bird", CarcassFeature::new);
+    public static final DeferredHolder<Feature<?>, Feature<?>> DEER = FEATURES.register("carcass_deer", CarcassFeature::new);
+    public static final DeferredHolder<Feature<?>, Feature<?>> GOAT = FEATURES.register("carcass_goat", CarcassFeature::new);
 
     public BoneZoneForge() {
 
